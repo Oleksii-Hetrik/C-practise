a87cd41a938a5ce1ca1c685d5ce1cec4615533e3
Oleksii-Hetrik <oleksii.hetrik@gmail.com>
2025-05-10 23:51:13 +0200

    error handling
    finished solution for N passengers

--- 3rd example of exam test/Npassangers.c
+++ 3rd example of exam test/Npassangers.c
@@ -29,7 +29,17 @@ int cmp(PASSENGER *a, PASSENGER *b)
 int main(int argc, char *argv[])
 {
     char line[77];
+    if (argc < 2)
+    {
+        printf("1st command-line argument is not present\n");
+        return 3;
+    }
     FILE *file = fopen(argv[1], "r");
+    if(!file)
+    {
+        printf("file %s cannot be opened\n", argv[1]);
+        return 4;
+    }
     int number_of_lines = atoi(fgets(line, sizeof(line), file));
     PASSENGER passengers[number_of_lines];
 
@@ -45,8 +55,18 @@ int main(int argc, char *argv[])
 
     qsort(passengers, number_of_lines, sizeof(PASSENGER), cmp);
 
+    if (argc < 3)
+    {
+        printf("2nd command-line argument is not present\n");
+        return 5;
+    }
     file = fopen(argv[2], "w");
     fprintf(file, "%d\n", number_of_lines);
+    if(!file)
+    {
+        printf("file %s cannot be opened\n", argv[2]);
+        return 7;
+    }
     for (int i = 0; i < number_of_lines; i++)
     {
         fprintf(file, "%s;%d;%s;%.2f\n", passengers[i].passport_number, passengers[i].age, passengers[i].full_name, passengers[i].baggage_weight);
