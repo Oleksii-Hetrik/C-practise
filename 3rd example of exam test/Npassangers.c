6b58e2e67f28e5409fd645612fc4da9c2f8f9a55
Oleksii-Hetrik <oleksii.hetrik@gmail.com>
2025-05-10 23:41:17 +0200

    unfinished 1 solution for exam
    N input passengers task
    I will need to add some error handling

---
+++ 3rd example of exam test/Npassangers.c
@@ -0,0 +1,57 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+typedef struct {
+    char passport_number[16];
+    int age;
+    char full_name[51];
+    double baggage_weight;
+} PASSENGER;
+
+int cmp(PASSENGER *a, PASSENGER *b)
+{
+    if(strcmp(a->full_name, b->full_name) != 0)
+    {
+        return - strcmp(a->full_name, b->full_name);
+    } else if(a->baggage_weight != b->baggage_weight)
+    {
+        return a->baggage_weight - b->baggage_weight;
+    } else if(a->age != b->age)
+    {
+        return a->age - b->age;
+    } else
+    {
+        return - strcmp(a->passport_number, b->passport_number);
+    }
+}
+
+int main(int argc, char *argv[])
+{
+    char line[77];
+    FILE *file = fopen(argv[1], "r");
+    int number_of_lines = atoi(fgets(line, sizeof(line), file));
+    PASSENGER passengers[number_of_lines];
+
+    for (int i = 0; i < number_of_lines; i++)
+    {
+        fgets(line, sizeof(line), file);
+        strcpy(passengers[i].passport_number, strtok(line, ";"));
+        passengers[i].age = atoi(strtok(NULL, ";"));
+        strcpy(passengers[i].full_name, strtok(NULL, ";"));
+        passengers[i].baggage_weight = atof(strtok(NULL, ";"));
+    }
+    fclose(file);
+
+    qsort(passengers, number_of_lines, sizeof(PASSENGER), cmp);
+
+    file = fopen(argv[2], "w");
+    fprintf(file, "%d\n", number_of_lines);
+    for (int i = 0; i < number_of_lines; i++)
+    {
+        fprintf(file, "%s;%d;%s;%.2f\n", passengers[i].passport_number, passengers[i].age, passengers[i].full_name, passengers[i].baggage_weight);
+    }
+
+    fclose(file);
+    return EXIT_SUCCESS;
+}
